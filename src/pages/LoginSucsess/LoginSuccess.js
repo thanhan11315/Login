a3fae8c1f62cd64dfbcbcf5a771211852f221987
anle <thanhan11315@gmail.com>
2022-08-13 15:20:46 +0700

    3

--- src/pages/LoginSucsess/LoginSuccess.js
+++ src/pages/LoginSucsess/LoginSuccess.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 function LoginSuccess() {
   const navigatea = useNavigate();
-  useEffect(() => {
-    navigatea("/LoginSuccess");
-  },[]);
+navigatea("/LoginSuccess");
   return (<h1>LoginSuccess233232</h1>);
 }
 
